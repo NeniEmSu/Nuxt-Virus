d8fcf2703336a90e82b11d43e3f4457888c6ddcd
Emmanuel Neni <emmanuelneni@gmail.com>
2019-05-03 18:21:00 +0300

    changed the routerBase for nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@
      host: '0.0.0.0',
    },
    router: {
-     base: '/<Nuxt-virus>/'
+     base: '/Nuxt-Virus/'
    },
    head: {
      title: "Детейлінг центр Virus Тернопіль. Хімчистка, полірування, реставрація авто",
