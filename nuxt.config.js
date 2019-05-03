79607537214be4a8c6133def87500266fe442555
Emmanuel Neni <emmanuelneni@gmail.com>
2019-05-03 18:18:23 +0300

    changed the routerBase for nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,14 @@
  import webpack from 'webpack'
 
- const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
-   router: {
-     base: '/<Nuxt-Virus>/'
-   }
- } : {}
-
  export default {
    mode: 'universal',
    server: {
      port: 8383,
      host: '0.0.0.0',
    },
-   ...routerBase,
+   router: {
+     base: '/<Nuxt-virus>/'
+   }
    head: {
      title: "Детейлінг центр Virus Тернопіль. Хімчистка, полірування, реставрація авто",
      htmlAttrs: {
