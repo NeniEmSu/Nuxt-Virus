e264abffb28e0227893f0750139aed7e3ca3f6c2
Emmanuel Neni <emmanuelneni@gmail.com>
2019-10-09 18:41:11 +0300

    minor fixes

--- pages/mahazyn/index.vue
+++ pages/mahazyn/index.vue
@@ -475,7 +475,7 @@ export default {
       'https://admin.virus.te.ua/api/collections/get/Product?token=9fc49d5af4dda3c961d71b489540a4&rspc=1',
       JSON.stringify({
         filter: { Published: true },
-        sort: { _created: -1 },
+        sort: { _created: 1 },
         populate: 1
       }),
       {
